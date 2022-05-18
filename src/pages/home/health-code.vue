9f15e53d8125751c9a40ebf7f4767ae686779195
yanmiao <yanmiao@shinemo.com>
2022-05-18 14:35:33 +0800

    feat : 健康码完成

--- src/pages/home/health-code.vue
+++ src/pages/home/health-code.vue
@@ -1,15 +1,146 @@
 <template>
   <view class="health-code">
-    健康码
+    <view class="code-box">
+      <view class="qrcode-time">
+        {{ qrcodeData.time }}
+      </view>
+      <view class="qrcode-box">
+        <ayQrcode
+            ref="qrcode"
+            :modal="modal_qr"
+            :themeColor="qrcodeData.color"
+            :url="qrcodeData.url"
+            qrcode_id="qrcode"
+            @hideQrcode="hideQrcode"/>
+      </view>
+      <view :style="{
+        color:qrcodeData.color,
+        background:qrcodeData.bgColor
+      }" class="qrcode-text">
+        {{ qrcodeData.text }}
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
+import ayQrcode from "../component/ay-qrcode/ay-qrcode.vue"
+
 export default {
-  name: "health-code"
+  name: "health-code",
+  components: {
+    ayQrcode
+  },
+  data() {
+    return {
+      //二维码相关参数
+      modal_qr: false,
+      qrcodeData: {
+        color: 'green',
+        bgColor: 'rgba(0, 255, 0, 0.1)',
+        url: "https://www.baidu.com",
+        text: '绿码',
+        time: this.getNowTime()
+      },
+      timer: null
+    }
+  },
+  mounted() {
+    this.qrcode()
+    this.timer = setInterval(() => {
+      this.qrcodeData.time = this.getNowTime()
+    }, 1000)
+  },
+  methods: {
+    qrcode() {
+      this.modal_qr = true;
+      // uni.showLoading()
+      setTimeout(() => {
+        // uni.hideLoading()
+        this.$refs.qrcode.crtQrCode()
+      }, 50)
+    },
+    //传入组件的方法
+    hideQrcode() {
+      this.modal_qr = false;
+    },
+
+    // 获取时间
+
+    //获取当前时间
+    getNowTime() {
+      let date = new Date();
+      //年 getFullYear()：四位数字返回年份
+      let year = date.getFullYear();  //getFullYear()代替getYear()
+      //月 getMonth()：0 ~ 11
+      let month = date.getMonth() + 1;
+      //日 getDate()：(1 ~ 31)
+      let day = date.getDate();
+      //时 getHours()：(0 ~ 23)
+      let hour = date.getHours();
+      //分 getMinutes()： (0 ~ 59)
+      let minute = date.getMinutes();
+      //秒 getSeconds()：(0 ~ 59)
+      let second = date.getSeconds();
+
+      return (
+          year
+          + '-' + this.zeroFill(month)
+          + '-' + this.zeroFill(day)
+          + ' ' + this.zeroFill(hour)
+          + ':' + this.zeroFill(minute)
+          + ':' + this.zeroFill(second)
+      )
+    },
+
+    // 补 0
+    zeroFill(num) {
+      return num.toString().padStart(2, "0")
+    }
+  },
+  deactivated() {
+    clearTimeout(this.timer)
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.health-code {
+  width: 100%;
+  height: 100vh;
+  background: #3B65E4;
+  padding-top: 100rpx;
 
+  .code-box {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border-radius: 10rpx;
+    width: 90%;
+    margin: 0 auto;
+    padding-bottom: 50rpx;
+
+    .qrcode-time {
+      width: 80%;
+      margin: 50rpx auto;
+      border-radius: 4px;
+      padding: 10rpx 20rpx;
+      border: 1px solid #ccc;
+      color: #333;
+      font-size: 36rpx;
+      text-align: center;
+      font-weight: 500;
+    }
+
+    .qrcode-text {
+      width: 40%;
+      display: block;
+      text-align: center;
+      margin: 0 auto;
+      padding: 10rpx 30rpx;
+      border-radius: 4px;
+      font-size: 30rpx;
+    }
+  }
+}
 </style>
--- src/pages/home/news.vue
+++ src/pages/home/news.vue
@@ -1,15 +1,43 @@
 <template>
   <view class="news">
-    小区头条
+    <u-cell-group>
+      <u-cell
+          v-for="(item,index) in cellList"
+          :key="index"
+          :isLink="true"
+          :title="item.title"
+          :url="item.path"
+          arrow-direction="right"
+          icon="file-text" size="large"/>
+    </u-cell-group>
   </view>
 </template>
 
 <script>
 export default {
-  name: "news"
+  name: "news",
+  data() {
+    return {
+      cellList: [
+        {title: '上海北蔡一小区发通知每单元派一名代表购物一小时？居委回应', path: ''},
+        {title: '海曙段塘：专业领域党建联盟助力 老旧小区改造按下"加速键"', path: ''},
+        {title: '十年芜湖世茂滨江小区逆生长', path: ''},
+        {title: '永定河孔雀城精品物业，专业缔造幸福生活', path: ''},
+        {title: '三个老旧小区上调物业费后 生活环境变好了 服务标准提高了', path: ''},
+        {title: '业主不交停车费，车子就不能进小区？', path: ''},
+        {title: '小区物业亟待建立服务评价和淘汰制度', path: ''},
+        {title: '佳兆业美好成都分公司“第一届邻里杯”社区趣味运动会圆满结束', path: ''},
+        {title: '云景东里社区携手物业开展底商消防安全培训活动', path: ''},
+        {title: '武汉公布物业服务小区双评议前十和后十名单 看看有没有你家的小区？', path: ''},
+        {title: '业委会赶走物业自治，2万电费没交被断电，业主：电梯都用不了', path: ''},
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+::v-deep .u-cell__body {
+  padding: 30rpx 15px;
+}
 </style>
